8f650bcbb7b399088b7e628fb6087f03acdb794b
jonathan-kelvin <jonathankelvinsantoso@gmail.com>
2022-03-17 01:46:15 +0800

    cloning

---
+++ src/App/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import faker from "@faker-js/faker";
+
+//* Styles
+import './style.scss'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "left",
+    },
+    // title: {
+    //   display: true,
+    //   text: "Bitcoin",
+    // },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Real Price",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Predicted Price",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="App__Header">Bitcoin NLP</h1>
+      <div className="App__LineContainer">
+        <Line options={options} data={data} />
+        <div>Bitcoin Price:</div>
+      </div>
+    </div>
+  );
+}
